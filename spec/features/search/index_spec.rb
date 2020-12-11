e1feed02702023e8cd1e2132809dfd500cbce0ca
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-11 10:33:53 -0700

    Update test for search index page

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -14,6 +14,12 @@ RSpec.describe 'Search results index page' do
           it "I should be on page '/search'" do
             expect(current_path).to eq('/search')
           end
+
+          it 'I see a total number of 20 ppeople who live in the fire nation' do
+            people = all('.people')
+            expect(people).to_not be_empty
+            expect(people.count).to eq(20)
+          end
         end
       end
     end
